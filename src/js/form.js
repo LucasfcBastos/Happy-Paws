db175835cd4d617053c0f0fe03a9ae0022c807df
LucasfcBastos <lucas.software.engineering@gmail.com>
2024-08-25 15:02:38 -0300

    front-end: new animal registration screen

---
+++ src/js/form.js
@@ -0,0 +1,21 @@
+/* IMG */
+function handleFileChange(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('preview');
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = 'svg/placeholder.svg';
+    }
+}
+
+/* Phone */
+document.getElementById('phone').addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
